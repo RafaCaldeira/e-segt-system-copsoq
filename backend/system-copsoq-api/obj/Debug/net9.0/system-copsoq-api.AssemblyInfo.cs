93321abd3d19b4c6be8f678b75a388428f590638
Luiz Rafael Jacques Caldeira <rafaelhos2016@gmail.com>
2026-04-12 16:01:56 -0300

    Feat: Arrumando o render e o supabase 7

--- backend/system-copsoq-api/obj/Debug/net9.0/system-copsoq-api.AssemblyInfo.cs
+++ backend/system-copsoq-api/obj/Debug/net9.0/system-copsoq-api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("system-copsoq-api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc01c32a2fc9901d26037ae77f978d8b8cd5a88e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+556d24240707d26fe902761ac03da751ec7ff638")]
 [assembly: System.Reflection.AssemblyProductAttribute("system-copsoq-api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("system-copsoq-api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
